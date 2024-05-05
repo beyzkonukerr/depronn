21bd6e7a32946f40b81f7a130d1f3dffca19d5dd
Mete Şahan KURT <137499436+metesahankurt@users.noreply.github.com>
2024-05-05 15:27:47 +0300

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,5 +1,4 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
@@ -11,13 +10,13 @@ export function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="posts/dynamic-routing"
+              href="/posts/dynamic-routing"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Ekibimiz
             </a>
             <a
-              href={`https://www.youtube.com/@ROSAT-qr5gc`}
+              href="https://www.youtube.com/@ROSAT-qr5gc"
               className="mx-3 font-bold hover:underline"
             >
               Youtube
