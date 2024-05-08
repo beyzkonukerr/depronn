c85a9c8ff23df06fe275ab1ffb3f896bd2c9bcd6
beyzkonukerr <bkonuker@gmail.com>
2024-05-08 21:29:43 +0300

    tamam

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,4 +1,5 @@
 import Container from "@/app/_components/container";
+import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
@@ -10,13 +11,13 @@ export function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="/posts/dynamic-routing"
+              href="/posts/ekibimiz" // Doğru URL'yi buraya ayarladık
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Ekibimiz
             </a>
             <a
-              href="https://www.youtube.com/@ROSAT-qr5gc"
+              href={`https://www.youtube.com/@ROSAT-qr5gc`}
               className="mx-3 font-bold hover:underline"
             >
               Youtube
